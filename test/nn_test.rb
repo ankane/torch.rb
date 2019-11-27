f50b5d4f88192ef6ad396455498aa521d018d921
Andrew Kane <andrew@chartkick.com>
2019-11-26 23:42:54 -0800

    Improved NN test [skip ci]

--- test/nn_test.rb
+++ test/nn_test.rb
@@ -1,7 +1,8 @@
 require_relative "test_helper"
 
 class NNTest < Minitest::Test
-  def test_works
+  # https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
+  def test_tutorial
     net = Net.new
 
     params = net.parameters
@@ -14,5 +15,21 @@ class NNTest < Minitest::Test
 
     net.zero_grad
     out.backward(Torch.randn(1, 10))
+
+    output = net.call(input)
+    target = Torch.randn(10)
+    target = target.view(1, -1)
+    criterion = Torch::NN::MSELoss.new
+    loss = criterion.call(output, target)
+
+    net.zero_grad
+    net.conv1.bias.grad
+    loss.backward
+    net.conv1.bias.grad
+
+    learning_rate = 0.01
+    net.parameters.each do |f|
+      f.data.sub!(f.grad.data * learning_rate)
+    end
   end
 end
