f89a852af927edea5360155418783accd629e3ca
Andrew Kane <andrew@chartkick.com>
2019-11-30 10:09:05 -0800

    Test ReLU [skip ci]

--- test/activations_test.rb
+++ test/activations_test.rb
@@ -1,6 +1,12 @@
 require_relative "test_helper"
 
 class ActivationsTest < Minitest::Test
+  def test_relu
+    m = Torch::NN::ReLU.new
+    input = Torch.randn(2)
+    output = m.call(input)
+  end
+
   def test_sigmoid
     m = Torch::NN::Sigmoid.new
     input = Torch.randn(2)
