8d9b593f66653ee94ce190750b4e92e42461d9cd
Andrew Kane <andrew@chartkick.com>
2019-11-30 06:48:13 -0800

    Set up loss tests

--- test/loss_test.rb
+++ test/loss_test.rb
@@ -1,6 +1,21 @@
 require_relative "test_helper"
 
 class LossTest < Minitest::Test
+  def test_bce_loss
+    skip
+    assert_works Torch::NN::BCELoss, :float
+  end
+
+  def test_bce_with_logits_loss
+    skip
+    assert_works Torch::NN::BCEWithLogitsLoss, :float
+  end
+
+  def test_cosine_embedding_loss
+    skip
+    assert_works Torch::NN::CosineEmbeddingLoss, :float
+  end
+
   def test_cross_entropy_loss
     assert_works Torch::NN::CrossEntropyLoss, :long
   end
@@ -22,20 +37,65 @@ class LossTest < Minitest::Test
     loss.backward
   end
 
-  def test_poisson_nll_loss
-    assert_works Torch::NN::PoissonNLLLoss, :float
+  def test_hinge_embedding_loss
+    skip
+    assert_works Torch::NN::HingeEmbeddingLoss, :float
+  end
+
+  def test_kl_div_loss
+    skip
+    assert_works Torch::NN::KLDivLoss, :float
   end
 
   def test_l1_loss
     assert_works Torch::NN::L1Loss, :float
   end
 
+  def test_margin_ranking_loss
+    skip
+    assert_works Torch::NN::MarginRankingLoss, :float
+  end
+
   def test_mse_loss
     assert_works Torch::NN::MSELoss, :float
   end
 
+  def test_multi_label_margin_loss
+    skip
+    assert_works Torch::NN::MultiLabelMarginLoss, :float
+  end
+
+  def test_multi_label_soft_margin_loss
+    skip
+    assert_works Torch::NN::MultiLabelSoftMarginLoss, :float
+  end
+
+  def test_multi_margin_loss
+    skip
+    assert_works Torch::NN::MultiMarginLoss, :float
+  end
+
   def test_nll_loss
     assert_works Torch::NN::NLLLoss, :long
+  end
+
+  def test_poisson_nll_loss
+    assert_works Torch::NN::PoissonNLLLoss, :float
+  end
+
+  def test_smooth_l1_loss
+    skip
+    assert_works Torch::NN::SmoothL1Loss, :float
+  end
+
+  def test_soft_margin_loss
+    skip
+    assert_works Torch::NN::SoftMarginLoss, :float
+  end
+
+  def test_triplet_margin_loss
+    skip
+    assert_works Torch::NN::TripletMarginLoss, :float
   end
 
   private
