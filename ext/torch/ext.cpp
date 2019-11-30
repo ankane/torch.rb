4641fdbecc23d66d23e7f39bcc8702cdf78a5e56
Andrew Kane <andrew@chartkick.com>
2019-11-30 06:30:04 -0800

    Cleaner tensor code

--- ext/torch/ext.cpp
+++ ext/torch/ext.cpp
@@ -150,7 +150,7 @@ void Init_ext()
       })
     .define_singleton_method(
       "floating_point?",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::is_floating_point(input);
       })
     .define_singleton_method(
@@ -222,32 +222,32 @@ void Init_ext()
     // begin operations
     .define_singleton_method(
       "_mean",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::mean(input);
       })
     .define_singleton_method(
       "_mean_dim",
-      *[](torch::Tensor& input, int64_t dim, bool keepdim) {
+      *[](Tensor& input, int64_t dim, bool keepdim) {
         return torch::mean(input, dim, keepdim);
       })
     .define_singleton_method(
       "_sum",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::sum(input);
       })
     .define_singleton_method(
       "_sum_dim",
-      *[](torch::Tensor& input, int64_t dim, bool keepdim) {
+      *[](Tensor& input, int64_t dim, bool keepdim) {
         return torch::sum(input, dim, keepdim);
       })
     .define_singleton_method(
       "_argmax",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::argmax(input);
       })
     .define_singleton_method(
       "_argmax_dim",
-      *[](torch::Tensor& input, int64_t dim, bool keepdim) {
+      *[](Tensor& input, int64_t dim, bool keepdim) {
         return torch::argmax(input, dim, keepdim);
       })
     .define_singleton_method(
@@ -257,226 +257,226 @@ void Init_ext()
       })
     .define_singleton_method(
       "_norm",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::norm(input);
       })
     .define_singleton_method(
       "_min",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::min(input);
       })
     .define_singleton_method(
       "_max",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::max(input);
       })
     .define_singleton_method(
       "_max_out",
-      *[](torch::Tensor &max, torch::Tensor &max_indices, const torch::Tensor &input, int64_t dim, bool keepdim) {
+      *[](Tensor &max, Tensor &max_indices, const Tensor &input, int64_t dim, bool keepdim) {
         // TODO add return value
         torch::_max_out(max, max_indices, input, dim, keepdim);
       })
     .define_singleton_method(
       "_sqrt",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::sqrt(input);
       })
     .define_singleton_method(
       "_exp",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::exp(input);
       })
     .define_singleton_method(
       "_log",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::log(input);
       })
     .define_singleton_method(
       "_sign",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::sign(input);
       })
     .define_singleton_method(
       "_unsqueeze",
-      *[](torch::Tensor& input, int64_t dim) {
+      *[](Tensor& input, int64_t dim) {
         return torch::unsqueeze(input, dim);
       })
     .define_singleton_method(
       "_dot",
-      *[](torch::Tensor& input, torch::Tensor& tensor) {
+      *[](Tensor& input, Tensor& tensor) {
         return torch::dot(input, tensor);
       })
     .define_singleton_method(
       "_matmul",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::matmul(input, other);
       })
     .define_singleton_method(
       "_eq",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::eq(input, other);
       })
     .define_singleton_method(
       "_gt",
       // TODO support tensors
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::gt(input, other);
       })
     .define_singleton_method(
       "_lt",
       // TODO support tensors
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::lt(input, other);
       })
     .define_singleton_method(
       "_add",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::add(input, other);
       })
     .define_singleton_method(
       "_add_scalar",
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::add(input, other);
       })
     .define_singleton_method(
       "_add_out",
-      *[](torch::Tensor& out, torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& out, Tensor& input, Tensor& other) {
         return torch::add_out(out, input, other);
       })
     .define_singleton_method(
       "_sub",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::sub(input, other);
       })
     .define_singleton_method(
       "_sub_scalar",
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::sub(input, other);
       })
     .define_singleton_method(
       "_mul",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::mul(input, other);
       })
     .define_singleton_method(
       "_mul_scalar",
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::mul(input, other);
       })
     .define_singleton_method(
       "_div",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::div(input, other);
       })
     .define_singleton_method(
       "_div_scalar",
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::div(input, other);
       })
     .define_singleton_method(
       "_remainder",
-      *[](torch::Tensor& input, torch::Tensor& other) {
+      *[](Tensor& input, Tensor& other) {
         return torch::remainder(input, other);
       })
     .define_singleton_method(
       "_remainder_scalar",
-      *[](torch::Tensor& input, Scalar other) {
+      *[](Tensor& input, Scalar other) {
         return torch::remainder(input, other);
       })
     .define_singleton_method(
       "_pow",
-      *[](torch::Tensor& input, Scalar exponent) {
+      *[](Tensor& input, Scalar exponent) {
         return torch::pow(input, exponent);
       })
     .define_singleton_method(
       "_abs",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::abs(input);
       })
     .define_singleton_method(
       "_neg",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::neg(input);
       })
     .define_singleton_method(
       "_reshape",
-      *[](torch::Tensor& input, IntArrayRef shape) {
+      *[](Tensor& input, IntArrayRef shape) {
         return torch::reshape(input, shape);
       })
     .define_singleton_method(
       "_flatten",
-      *[](torch::Tensor& input, int64_t start_dim, int64_t end_dim) {
+      *[](Tensor& input, int64_t start_dim, int64_t end_dim) {
         return torch::flatten(input, start_dim, end_dim);
       })
     .define_singleton_method(
       "relu",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::relu(input);
       })
     .define_singleton_method(
       "conv2d",
-      *[](torch::Tensor& input, torch::Tensor& weight, torch::Tensor& bias, IntArrayRef stride, IntArrayRef padding, IntArrayRef dilation, int64_t groups) {
+      *[](Tensor& input, Tensor& weight, Tensor& bias, IntArrayRef stride, IntArrayRef padding, IntArrayRef dilation, int64_t groups) {
         return torch::conv2d(input, weight, bias, stride, padding, dilation, groups);
       })
     .define_singleton_method(
       "linear",
-      *[](torch::Tensor& input, torch::Tensor& weight, torch::Tensor& bias) {
+      *[](Tensor& input, Tensor& weight, Tensor& bias) {
         return torch::linear(input, weight, bias);
       })
     .define_singleton_method(
       "max_pool2d",
-      *[](torch::Tensor& input, IntArrayRef kernel_size) {
+      *[](Tensor& input, IntArrayRef kernel_size) {
         return torch::max_pool2d(input, kernel_size);
       })
     .define_singleton_method(
       "avg_pool2d",
-      *[](torch::Tensor& input, IntArrayRef kernel_size) {
+      *[](Tensor& input, IntArrayRef kernel_size) {
         return torch::avg_pool2d(input, kernel_size);
       })
     .define_singleton_method(
       "_dropout",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::dropout(input, p, train);
       })
     .define_singleton_method(
       "_dropout!",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::dropout_(input, p, train);
       })
     .define_singleton_method(
       "_feature_dropout",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::feature_dropout(input, p, train);
       })
     .define_singleton_method(
       "_feature_dropout!",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::feature_dropout_(input, p, train);
       })
     .define_singleton_method(
       "_alpha_dropout",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::alpha_dropout(input, p, train);
       })
     .define_singleton_method(
       "_alpha_dropout!",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::alpha_dropout_(input, p, train);
       })
     .define_singleton_method(
       "_feature_alpha_dropout",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::feature_alpha_dropout(input, p, train);
       })
     .define_singleton_method(
       "_feature_alpha_dropout!",
-      *[](torch::Tensor& input, float p, bool train) {
+      *[](Tensor& input, float p, bool train) {
         return torch::feature_alpha_dropout_(input, p, train);
       })
     .define_singleton_method(
       "_embedding",
       // weight and indices are swapped from Python interface
-      *[](const torch::Tensor &indices, const torch::Tensor &weight, int64_t padding_idx, bool scale_grad_by_freq, bool sparse) {
+      *[](const Tensor &indices, const Tensor &weight, int64_t padding_idx, bool scale_grad_by_freq, bool sparse) {
         return torch::embedding(weight, indices, padding_idx, scale_grad_by_freq, sparse);
       })
     // loss functions
@@ -488,19 +488,19 @@ void Init_ext()
       })
     .define_singleton_method(
       "l1_loss",
-      *[](torch::Tensor& input, torch::Tensor& target, std::string reduction) {
+      *[](Tensor& input, Tensor& target, std::string reduction) {
         auto red = reduction == "mean" ? Reduction::Mean : Reduction::Sum;
         return torch::l1_loss(input, target, red);
       })
     .define_singleton_method(
       "mse_loss",
-      *[](torch::Tensor& input, torch::Tensor& target, std::string reduction) {
+      *[](Tensor& input, Tensor& target, std::string reduction) {
         auto red = reduction == "mean" ? Reduction::Mean : Reduction::Sum;
         return torch::mse_loss(input, target, red);
       })
     .define_singleton_method(
       "nll_loss",
-      *[](torch::Tensor& input, torch::Tensor& target, std::string reduction, int64_t ignore_index) {
+      *[](Tensor& input, Tensor& target, std::string reduction, int64_t ignore_index) {
         auto red = reduction == "mean" ? Reduction::Mean : Reduction::Sum;
         return torch::nll_loss(input, target, {}, red, ignore_index);
       })
@@ -536,171 +536,171 @@ void Init_ext()
     .define_method("view_as", &torch::Tensor::view_as)
     .define_method(
       "addcmul!",
-      *[](torch::Tensor& self, Scalar value, const torch::Tensor & tensor1, const torch::Tensor & tensor2) {
+      *[](Tensor& self, Scalar value, const Tensor & tensor1, const Tensor & tensor2) {
         return self.addcmul_(tensor1, tensor2, value);
       })
     .define_method(
       "addcdiv!",
-      *[](torch::Tensor& self, Scalar value, const torch::Tensor & tensor1, const torch::Tensor & tensor2) {
+      *[](Tensor& self, Scalar value, const Tensor & tensor1, const Tensor & tensor2) {
         return self.addcdiv_(tensor1, tensor2, value);
       })
     .define_method(
       "zero!",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.zero_();
       })
     .define_method(
       "detach",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.detach();
       })
     .define_method(
       "detach!",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.detach_();
       })
     .define_method(
       "_select",
-      *[](torch::Tensor& self, int64_t dim, int64_t index) {
+      *[](Tensor& self, int64_t dim, int64_t index) {
         return self.select(dim, index);
       })
     .define_method(
       "_slice",
-      *[](torch::Tensor& self, int64_t dim, int64_t start, int64_t end, int64_t step) {
+      *[](Tensor& self, int64_t dim, int64_t start, int64_t end, int64_t step) {
         return self.slice(dim, start, end, step);
       })
     .define_method(
       "_requires_grad!",
-      *[](torch::Tensor& self, bool requires_grad) {
+      *[](Tensor& self, bool requires_grad) {
         return self.set_requires_grad(requires_grad);
       })
     .define_method(
       "_backward",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.backward();
       })
     .define_method(
       "_backward_gradient",
-      *[](torch::Tensor& self, const torch::Tensor& gradient) {
+      *[](Tensor& self, const Tensor& gradient) {
         return self.backward(gradient);
       })
     .define_method(
       "grad",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.grad();
       })
     .define_method(
       "_dtype",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return (int) at::typeMetaToScalarType(self.dtype());
       })
     .define_method(
       "_type",
-      *[](torch::Tensor& self, int dtype) {
+      *[](Tensor& self, int dtype) {
         return self.toType((torch::ScalarType) dtype);
       })
     .define_method(
       "_layout",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         std::stringstream s;
         s << self.layout();
         return s.str();
       })
     .define_method(
       "device",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         std::stringstream s;
         s << self.device();
         return s.str();
       })
     .define_method(
       "_view",
-      *[](torch::Tensor& self, IntArrayRef size) {
+      *[](Tensor& self, IntArrayRef size) {
         return self.view(size);
       })
     .define_method(
       "resize_as!",
-      *[](torch::Tensor& self, torch::Tensor& other) {
+      *[](Tensor& self, Tensor& other) {
         return self.resize_as_(other);
       })
     .define_method(
       "fill!",
-      *[](torch::Tensor& self, Scalar value) {
+      *[](Tensor& self, Scalar value) {
         return self.fill_(value);
       })
     .define_method(
       "_add!",
-      *[](torch::Tensor& self, torch::Tensor& other) {
+      *[](Tensor& self, Tensor& other) {
         return self.add_(other);
       })
     .define_method(
       "_add_alpha!",
-      *[](torch::Tensor& self, torch::Tensor& other, Scalar alpha) {
+      *[](Tensor& self, Tensor& other, Scalar alpha) {
         return self.add_(other, alpha);
       })
     .define_method(
       "_add_scalar!",
-      *[](torch::Tensor& self, Scalar other) {
+      *[](Tensor& self, Scalar other) {
         return self.add_(other);
       })
     .define_method(
       "normal!",
-      *[](torch::Tensor& self, double mean, double std) {
+      *[](Tensor& self, double mean, double std) {
         return self.normal_(mean, std);
       })
     .define_method(
       "sub!",
-      *[](torch::Tensor& self, torch::Tensor& other) {
+      *[](Tensor& self, Tensor& other) {
         return self.sub_(other);
       })
     .define_method(
       "_mul!",
-      *[](torch::Tensor& self, torch::Tensor& other) {
+      *[](Tensor& self, Tensor& other) {
         return self.mul_(other);
       })
     .define_method(
       "_mul_scalar!",
-      *[](torch::Tensor& self, Scalar other) {
+      *[](Tensor& self, Scalar other) {
         return self.mul_(other);
       })
     .define_method(
       "div!",
-      *[](torch::Tensor& self, torch::Tensor& other) {
+      *[](Tensor& self, Tensor& other) {
         return self.div_(other);
       })
     .define_method(
       "sqrt!",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.sqrt_();
       })
     .define_method(
       "unsqueeze!",
-      *[](torch::Tensor& self, int64_t dim) {
+      *[](Tensor& self, int64_t dim) {
         return self.unsqueeze_(dim);
       })
     .define_method(
       "copy!",
-      *[](torch::Tensor& self, torch::Tensor& src) {
+      *[](Tensor& self, Tensor& src) {
         return self.copy_(src);
       })
     .define_method(
       "clone",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.clone();
       })
     .define_method(
       "log_softmax",
-      *[](torch::Tensor& self, int64_t dim) {
+      *[](Tensor& self, int64_t dim) {
         return self.log_softmax(dim);
       })
     .define_method(
       "data",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         return self.data();
       })
     .define_method(
       "_data",
-      *[](torch::Tensor& self) {
+      *[](Tensor& self) {
         Array a;
         auto dtype = self.dtype();
 
@@ -752,17 +752,17 @@ void Init_ext()
       })
     .define_method(
       "_size",
-      *[](torch::Tensor& self, int i) {
+      *[](Tensor& self, int i) {
         return self.size(i);
       })
     .define_method(
       "_to",
-      *[](torch::Tensor& self, torch::Device device, int dtype, bool non_blocking, bool copy) {
+      *[](Tensor& self, torch::Device device, int dtype, bool non_blocking, bool copy) {
         return self.to(device, (torch::ScalarType) dtype, non_blocking, copy);
       })
     .define_singleton_method(
       "_make_subclass",
-      *[](torch::Tensor& rd, bool requires_grad) {
+      *[](Tensor& rd, bool requires_grad) {
         auto data = torch::autograd::as_variable_ref(rd).detach();
         data.unsafeGetTensorImpl()->set_allow_tensor_metadata_change(true);
         auto var = data.set_requires_grad(requires_grad);
@@ -814,17 +814,17 @@ void Init_ext()
   Module rb_mInit = define_module_under(rb_mNN, "Init")
     .define_singleton_method(
       "kaiming_uniform!",
-      *[](torch::Tensor& input, double a) {
+      *[](Tensor& input, double a) {
         return torch::nn::init::kaiming_uniform_(input, a);
       })
     .define_singleton_method(
       "normal!",
-      *[](torch::Tensor& input) {
+      *[](Tensor& input) {
         return torch::nn::init::normal_(input);
       })
     .define_singleton_method(
       "uniform!",
-      *[](torch::Tensor& input, double to, double from) {
+      *[](Tensor& input, double to, double from) {
         return torch::nn::init::uniform_(input, to, from);
       });
 
