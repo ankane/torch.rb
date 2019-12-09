e098c7d8ea59a1d8a7778a83b3340ce72bc54d0b
Andrew Kane <andrew@chartkick.com>
2019-12-09 13:23:29 -0800

    Added more pooling tests [skip ci]

--- test/nn/pooling_layers_test.rb
+++ test/nn/pooling_layers_test.rb
@@ -74,6 +74,15 @@ class PoolingLayersTest < Minitest::Test
     output = m.call(input)
   end
 
+  def test_fractional_max_pool2d
+    skip "Not implemented yet"
+
+    m = Torch::NN::FractionalMaxPool2d.new(3, output_size: [13, 12])
+    m = Torch::NN::FractionalMaxPool2d.new(3, output_ratio: [0.5, 0.5])
+    input = Torch.randn(20, 16, 50, 32)
+    output = m.call(input)
+  end
+
   def test_lp_pool1d
     skip "Not implemented yet"
 
@@ -88,6 +97,86 @@ class PoolingLayersTest < Minitest::Test
     m = Torch::NN::LPPool2d.new(2, 3, stride: 2)
     m = Torch::NN::LPPool2d.new(1.2, [3, 2], stride: [2, 1])
     input = Torch.randn(20, 16, 50, 32)
+    output = m.call(input)
+  end
+
+  def test_adaptive_max_pool1d
+    skip "Not implemented yet"
+
+    m = Torch::NN::AdaptiveMaxPool1d.new(5)
+    input = Torch.randn(1, 64, 8)
+    output = m.call(input)
+  end
+
+  def test_adaptive_max_pool2d
+    skip "Not implemented yet"
+
+    m = Torch::NN::AdaptiveMaxPool2d.new([5, 7])
+    input = Torch.randn(1, 64, 8, 9)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveMaxPool2d.new(7)
+    input = Torch.randn(1, 64, 10, 9)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveMaxPool2d.new([nil, 7])
+    input = Torch.randn(1, 64, 10, 9)
+    output = m.call(input)
+  end
+
+  def test_adaptive_max_pool3d
+    skip "Not implemented yet"
+
+    m = Torch::NN::AdaptiveMaxPool3d.new([5, 7, 9])
+    input = Torch.randn(1, 64, 8, 9, 10)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveMaxPool3d.new(7)
+    input = Torch.randn(1, 64, 10, 9, 8)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveMaxPool3d.new([7, nil, nil])
+    input = Torch.randn(1, 64, 10, 9, 8)
+    output = m.call(input)
+  end
+
+  def test_adaptive_avg_pool1d
+    skip "Not implemented yet"
+
+    m = Torch::NN::AdaptiveAvgPool1d.new(5)
+    input = Torch.randn(1, 64, 8)
+    output = m.call(input)
+  end
+
+  def test_adaptive_avg_pool2d
+    skip "Not implemented yet"
+
+    m = Torch::NN::AdaptiveAvgPool2d.new([5, 7])
+    input = Torch.randn(1, 64, 8, 9)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveAvgPool2d.new(7)
+    input = Torch.randn(1, 64, 10, 9)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveAvgPool2d.new([nil, 7])
+    input = Torch.randn(1, 64, 10, 9)
+    output = m.call(input)
+  end
+
+  def test_adaptive_avg_pool3d
+    skip "Not implemented yet"
+
+    m = Torch::NN::AdaptiveAvgPool3d.new([5, 7, 9])
+    input = Torch.randn(1, 64, 8, 9, 10)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveAvgPool3d.new(7)
+    input = Torch.randn(1, 64, 10, 9, 8)
+    output = m.call(input)
+
+    m = Torch::NN::AdaptiveMaxPool3d.new([7, nil, nil])
+    input = Torch.randn(1, 64, 10, 9, 8)
     output = m.call(input)
   end
 end
