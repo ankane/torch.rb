9ac672fb20ba882df25d4358ee7e99b6e560871c
Andrew Kane <andrew@chartkick.com>
2019-12-08 23:13:46 -0800

    Added convolution layers tests [skip ci]

--- test/nn/convolution_layers_test.rb
+++ test/nn/convolution_layers_test.rb
@@ -1,11 +1,75 @@
 require_relative "../test_helper"
 
 class ConvolutionLayersTest < Minitest::Test
+  def test_conv1d
+    skip "Not implemented yet"
+
+    m = Torch::NN::Conv1d.new(16, 33, 3, stride: 2)
+    input = Torch.randn(20, 16, 50)
+    output = m.call(input)
+  end
+
   def test_conv2d
     m = Torch::NN::Conv2d.new(16, 33, 3, stride: 2)
     m = Torch::NN::Conv2d.new(16, 33, [3, 5], stride: [2, 1], padding: [4, 2])
     m = Torch::NN::Conv2d.new(16, 33, [3, 5], stride: [2, 1], padding: [4, 2], dilation: [3, 1])
     input = Torch.randn(20, 16, 50, 100)
     output = m.call(input)
+  end
+
+  def test_conv3d
+    skip "Not implemented yet"
+
+    m = Torch::NN::Conv3d.new(16, 33, 3, stride: 2)
+    m = Torch::NN::Conv3d.new(16, 33, [3, 5, 2], stride: [2, 1, 1], padding: [4, 2, 0])
+    input = Torch.randn(20, 16, 10, 50, 100)
+    output = m.call(input)
+  end
+
+  def test_conv_transpose1d
+    skip "No test"
+  end
+
+  def test_conv_transpose2d
+    skip "Not implemented yet"
+
+    m = Torch::NN::ConvTranspose2d.new(16, 33, 3, stride: 2)
+    m = Torch::NN::ConvTranspose2d.new(16, 33, [3, 5], stride: [2, 1], padding: [4, 2])
+    input = Torch.randn(20, 16, 50, 100)
+    output = m.call(input)
+    input = Torch.randn(1, 16, 12, 12)
+    downsample = Torch::NN::Conv2d.new(16, 16, 3, stride: 2, padding: 1)
+    upsample = Torch::NN::ConvTranspose2d.new(16, 16, 3, stride: 2, padding: 1)
+    h = downsample.call(input)
+    assert_equal [1, 16, 6, 6], h.size
+    output = upsample.call(h, output_size: input.size())
+    assert_equal [1, 16, 12, 12], output.size
+  end
+
+  def test_conv_transpose3d
+    skip "Not implemented yet"
+
+    m = Torch::NN::ConvTranspose3d.new(16, 33, 3, stride: 2)
+    m = Torch::NN::ConvTranspose3d.new(16, 33, [3, 5, 2], stride: [2, 1, 1], padding: [0, 4, 2])
+    input = Torch.randn(20, 16, 10, 50, 100)
+    output = m.call(input)
+  end
+
+  def test_unfold
+    skip "Not implemented yet"
+
+    unfold =  Torch::NN::Unfold.new(kernel_size: [2, 3])
+    input = Torch.randn(2, 5, 3, 4)
+    output = unfold.call(input)
+    assert_equal [2, 30, 4], output.size
+  end
+
+  def test_fold
+    skip "Not implemented yet"
+
+    fold = Torch::NN::Fold.new(output_size: [4, 5], kernel_size: [2, 2])
+    input = Torch.randn(1, 3 * 2 * 2, 12)
+    output = fold.call(input)
+    assert_equal [1, 3, 4, 5], output.size
   end
 end
