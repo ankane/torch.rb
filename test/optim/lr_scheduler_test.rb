89a8ea46d89d5e76dc54f541c06dab955ca21a27
Andrew Kane <andrew@chartkick.com>
2020-01-28 23:20:10 -0800

    Added LambdaLR scheduler

--- test/optim/lr_scheduler_test.rb
+++ test/optim/lr_scheduler_test.rb
@@ -1,15 +1,28 @@
 require_relative "../test_helper"
 
 class LRSchedulerTest < Minitest::Test
+  def test_lambda_lr
+    model = Net.new
+    optimizer = Torch::Optim::SGD.new(model.parameters, lr: 0.05)
+    lambda1 = ->(epoch) { 0.95 ** epoch }
+    scheduler = Torch::Optim::LRScheduler::LambdaLR.new(optimizer, [lambda1])
+    lrs = []
+    3.times do
+      scheduler.step
+      lrs << optimizer.param_groups[0][:lr]
+    end
+    assert_elements_in_delta [0.0475, 0.045125, 0.04286875], lrs
+  end
+
   def test_step_lr
     model = Net.new
     optimizer = Torch::Optim::SGD.new(model.parameters, lr: 0.05)
     scheduler = Torch::Optim::LRScheduler::StepLR.new(optimizer, step_size: 1, gamma: 0.1)
     lrs = []
     3.times do
-      lrs << optimizer.param_groups[0][:lr]
       scheduler.step
+      lrs << optimizer.param_groups[0][:lr]
     end
-    assert_elements_in_delta [0.05, 0.005, 0.0005], lrs
+    assert_elements_in_delta [0.005, 0.0005, 0.00005], lrs
   end
 end
