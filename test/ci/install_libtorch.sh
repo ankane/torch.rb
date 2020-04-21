204947d5e2682159ddf5cde64872d509a8bce15c
Andrew Kane <andrew@chartkick.com>
2020-04-21 16:28:16 -0700

    Test CUDA

--- test/ci/install_libtorch.sh
+++ test/ci/install_libtorch.sh
@@ -5,8 +5,8 @@ set -e
 CACHE_DIR=$HOME/libtorch/$LIBTORCH_VERSION
 
 if [ ! -d "$CACHE_DIR" ]; then
-  wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION%2Bcpu.zip
-  unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION+cpu.zip
+  wget https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION.zip
+  unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION.zip
   mv libtorch $CACHE_DIR
 else
   echo "LibTorch cached"
