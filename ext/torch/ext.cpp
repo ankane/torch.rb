7ef4a12bd3c9340e33c617d3444197cd3f5acf25
Andrew Kane <andrew@chartkick.com>
2020-04-21 14:32:09 -0700

    Removed device_count for now

--- ext/torch/ext.cpp
+++ ext/torch/ext.cpp
@@ -409,6 +409,6 @@ void Init_ext()
       });
 
   Module rb_mCUDA = define_module_under(rb_mTorch, "CUDA")
-    .define_singleton_method("available?", &torch::cuda::is_available)
-    .define_singleton_method("device_count", &torch::cuda::device_count);
+    .define_singleton_method("available?", &torch::cuda::is_available);
+    // .define_singleton_method("device_count", &torch::cuda::device_count);
 }
