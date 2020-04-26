f065fcd1a7614c3a70dc65c14e584015f2332026
Andrew Kane <andrew@chartkick.com>
2020-04-26 16:57:14 -0700

    Added support for GPU installation in test script [skip ci]

--- test/ci/install_libtorch.sh
+++ test/ci/install_libtorch.sh
@@ -5,8 +5,13 @@ set -e
 CACHE_DIR=$HOME/libtorch/$LIBTORCH_VERSION
 
 if [ ! -d "$CACHE_DIR" ]; then
-  wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION%2Bcpu.zip
-  unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION+cpu.zip
+  if [ -n "$LIBTORCH_GPU" ]; then
+    wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION%2Bcpu.zip
+    unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION+cpu.zip
+  else
+    wget https://download.pytorch.org/libtorch/$LIBTORCH_GPU/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION.zip
+    unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION.zip
+  fi
   mv libtorch $CACHE_DIR
 else
   echo "LibTorch cached"
