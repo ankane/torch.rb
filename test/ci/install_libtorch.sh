c245863e15b0614044df11a295e9100407116164
Andrew Kane <andrew@chartkick.com>
2020-04-26 17:06:00 -0700

    Fixed logic [skip ci]

--- test/ci/install_libtorch.sh
+++ test/ci/install_libtorch.sh
@@ -6,11 +6,11 @@ CACHE_DIR=$HOME/libtorch/$LIBTORCH_VERSION
 
 if [ ! -d "$CACHE_DIR" ]; then
   if [ -n "$LIBTORCH_GPU" ]; then
-    wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION%2Bcpu.zip
-    unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION+cpu.zip
-  else
     wget https://download.pytorch.org/libtorch/$LIBTORCH_GPU/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION.zip
     unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION.zip
+  else
+    wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION%2Bcpu.zip
+    unzip libtorch-cxx11-abi-shared-with-deps-$LIBTORCH_VERSION+cpu.zip
   fi
   mv libtorch $CACHE_DIR
 else
