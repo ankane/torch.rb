38c72bd9cab5f0b26c03b79ec96e6933a40b8eb6
Andrew Kane <andrew@chartkick.com>
2020-04-27 18:26:56 -0700

    Improved data loader

--- lib/torch/utils/data/data_loader.rb
+++ lib/torch/utils/data/data_loader.rb
@@ -12,14 +12,33 @@ module Torch
         end
 
         def each
+          max_size = @dataset.size
           size.times do |i|
             start_index = i * @batch_size
-            yield @dataset[start_index...(start_index + @batch_size)]
+            end_index = [start_index + @batch_size, max_size].min
+            batch = (end_index - start_index).times.map { |j| @dataset[start_index + j] }
+            yield collate(batch)
           end
         end
 
         def size
           (@dataset.size / @batch_size.to_f).ceil
+        end
+
+        private
+
+        def collate(batch)
+          elem = batch[0]
+          case elem
+          when Tensor
+            Torch.stack(batch, 0)
+          when Integer
+            Torch.tensor(batch)
+          when Array
+            batch.transpose.map { |v| collate(v) }
+          else
+            raise NotImpelmentYet
+          end
         end
       end
     end
