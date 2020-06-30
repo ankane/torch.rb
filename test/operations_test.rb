415a157d7001d207479f0d67ca4539f03a39b1a6
Andrew Kane <andrew@chartkick.com>
2020-06-29 23:36:51 -0700

    Moved tests [skip ci]

--- test/operations_test.rb
+++ test/operations_test.rb
@@ -151,52 +151,6 @@ class OperationsTest < Minitest::Test
     assert_equal [[1, 2, 3, 4], [5, 6, 7, 8]], Torch.flatten(x, start_dim: 1).to_a
   end
 
-  def test_type
-    x = Torch.ones([1, 2, 3])
-    assert_equal :float64, x.type(:float64).dtype
-    assert_equal :float64, x.double.dtype
-    assert_equal :int32, x.int.dtype
-    assert_equal :float32, x.dtype
-  end
-
-  def test_getter
-    x = Torch.tensor([[0, 1, 2], [3, 4, 5]])
-    assert_equal [0, 1, 2], x[0].to_a
-    assert_equal 5, x[1, 2].item
-    assert_equal [0, 1], x[0, 0..1].to_a
-    assert_equal [[[0, 1, 2], [3, 4, 5]]], x[true].to_a
-    assert_equal [[[0, 1, 2], [3, 4, 5]]], x[nil].to_a
-    # assert_equal [], x[false].to_a
-    # if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.6.0")
-    #   assert_equal [1, 2], x[0, eval("1..")].to_a
-    # end
-  end
-
-  def test_getter_tensor
-    x = Torch.tensor([1, 2, 3])
-    index = Torch.tensor([false, true, false])
-    assert_equal [2], x[index].to_a
-  end
-
-  def test_setter_numeric
-    x = Torch.tensor([1, 2, 3])
-    x[1] = 9
-    assert_equal [1, 9, 3], x.to_a
-  end
-
-  def test_setter_range
-    x = Torch.tensor([1, 2, 3])
-    x[1..2] = 9
-    assert_equal [1, 9, 9], x.to_a
-  end
-
-  def test_setter_tensor
-    x = Torch.tensor([1, 2, 3])
-    index = Torch.tensor([false, true, false])
-    x[index] = 9
-    assert_equal [1, 9, 3], x.to_a
-  end
-
   def test_setter_tensor_float
     x = Torch.tensor([1.0, 2, 3])
     index = Torch.tensor([false, true, false])
