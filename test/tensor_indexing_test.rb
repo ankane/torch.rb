9565f128be20f288f3b514bb97687d3c6e707fef
Andrew Kane <andrew@chartkick.com>
2020-09-22 04:07:51 -0700

    Moved tensor indexing tests to separate file [skip ci]

---
+++ test/tensor_indexing_test.rb
@@ -0,0 +1,70 @@
+require_relative "test_helper"
+
+class TensorIndexingTest < Minitest::Test
+  def test_getter
+    x = Torch.tensor([[0, 1, 2], [3, 4, 5]])
+    assert_equal [0, 1, 2], x[0].to_a
+    assert_equal 5, x[1, 2].item
+    assert_equal [0, 1], x[0, 0..1].to_a
+    assert_equal [[[0, 1, 2], [3, 4, 5]]], x[true].to_a
+    assert_equal [[[0, 1, 2], [3, 4, 5]]], x[nil].to_a
+    assert_equal [1, 2], x[0, 1..-1].to_a
+    assert_equal [1], x[0, 1...-1].to_a
+    assert_equal [0, 1], x[0, 0...-1].to_a
+    assert_equal [[0, 1, 2]], x[0...-1].to_a
+    assert_equal [], x[false].to_a
+    if RUBY_VERSION.to_f > 2.6
+      assert_equal [1, 2], x[0, eval("1..")].to_a
+      assert_equal [1], x[0, eval("(1...)")].to_a
+    end
+    if RUBY_VERSION.to_f > 2.7
+      assert_equal [0, 1], x[0, eval("..2")].to_a
+      assert_equal [0, 1, 2], x[0, eval("..")].to_a
+    end
+  end
+
+  def test_getter_tensor
+    x = Torch.tensor([1, 2, 3])
+    index = Torch.tensor([false, true, false])
+    assert_equal [2], x[index].to_a
+  end
+
+  def test_getter_large_integer
+    x = Torch.tensor([1, 2, 3])
+    error = assert_raises(RangeError) do
+      x[2**64]
+    end
+    assert_equal "bignum too big to convert into `long long'", error.message
+  end
+
+  def test_setter_numeric
+    x = Torch.tensor([1, 2, 3])
+    x[1] = 9
+    assert_equal [1, 9, 3], x.to_a
+  end
+
+  def test_setter_range_end
+    x = Torch.tensor([1, 2, 3])
+    x[1..2] = 9
+    assert_equal [1, 9, 9], x.to_a
+  end
+
+  def test_setter_range_end_negative
+    x = Torch.tensor([[0, 1, 2], [3, 4, 5]])
+    x[1, 1..-1] = 9
+    assert_equal [[0, 1, 2], [3, 9, 9]], x.to_a
+  end
+
+  def test_setter_range_end_negative_exclude_end
+    x = Torch.tensor([[0, 1, 2], [3, 4, 5]])
+    x[1, 1...-1] = 9
+    assert_equal [[0, 1, 2], [3, 9, 5]], x.to_a
+  end
+
+  def test_setter_tensor
+    x = Torch.tensor([1, 2, 3])
+    index = Torch.tensor([false, true, false])
+    x[index] = 9
+    assert_equal [1, 9, 3], x.to_a
+  end
+end
