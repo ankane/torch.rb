7316cd9326b47831e9e88c7ff668a8794c4d43a8
Andrew Kane <andrew@ankane.org>
2021-06-15 13:07:20 -0700

    Added comment [skip ci]

--- test/nn/pooling_layers_test.rb
+++ test/nn/pooling_layers_test.rb
@@ -8,6 +8,7 @@ class PoolingLayersTest < Minitest::Test
   end
 
   def test_max_pool2d
+    # warning in max_pool2d (same as PyTorch 1.9.0)
     m = Torch::NN::MaxPool2d.new(3, stride: 2)
     m = Torch::NN::MaxPool2d.new([3, 2], stride: [2, 1])
     input = Torch.randn(20, 16, 50, 32)
