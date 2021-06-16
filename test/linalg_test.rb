5ba872b4bdec868f0d23123022c6714909945706
Andrew Kane <andrew@ankane.org>
2021-06-15 19:56:20 -0700

    Added more tests

--- test/linalg_test.rb
+++ test/linalg_test.rb
@@ -2,7 +2,26 @@ require_relative "test_helper"
 
 class LinalgTest < Minitest::Test
   def test_norm
-    x = Torch.tensor([1.0, 2, 3])
-    assert_in_delta 3.7417, Torch::Linalg.norm(x).item
+    a = Torch.tensor([1.0, 2, 3])
+    assert_in_delta 3.7417, Torch::Linalg.norm(a).item
+  end
+
+  def test_vector_norm
+    a = Torch.arange(9, dtype: :float) - 4
+    b = a.reshape([3, 3])
+    assert_in_delta 5.4345, Torch::Linalg.vector_norm(a, ord: 3.5).item
+    assert_in_delta 5.4345, Torch::Linalg.vector_norm(b, ord: 3.5).item
+  end
+
+  def test_matrix_norm
+    a = Torch.arange(9, dtype: :float).reshape(3, 3)
+    # TOOD fix
+    # assert_in_delta 14.2829, Torch::Linalg.matrix_norm(a).item
+    # assert_in_delta 9, Torch::Linalg.matrix_norm(a, ord: -1).item
+  end
+
+  def test_det
+    a = Torch.arange(9, dtype: :float).reshape(3, 3)
+    assert_in_delta 0, Torch::Linalg.det(a).item
   end
 end
