b6829e9d679f3f0acba5f3069e4169ea282818bf
Andrew Kane <andrew@ankane.org>
2021-06-15 19:20:00 -0700

    Improved fft tests [skip ci]

--- test/fft_test.rb
+++ test/fft_test.rb
@@ -3,6 +3,11 @@ require_relative "test_helper"
 class FFTTest < Minitest::Test
   def test_fft
     t = Torch.arange(4)
-    assert_equal [6+0i, -2+2i, -2+0i, -2-2i], Torch::FFT.fft(t).to_a
+    assert_equal [6, -2+2i, -2, -2-2i], Torch::FFT.fft(t).to_a
+  end
+
+  def test_ifft
+    t = Torch.tensor([6, -2+2i, -2, -2-2i])
+    assert_equal [0, 1, 2, 3], Torch::FFT.ifft(t).to_a
   end
 end
