b78953c0d232c84f008364a90c529374e6beddca
Andrew Kane <andrew@ankane.org>
2022-01-23 16:53:57 -0800

    Fixed more test warnings [skip ci]

--- test/nn/activations_other_test.rb
+++ test/nn/activations_other_test.rb
@@ -4,24 +4,24 @@ class ActivationsOtherTest < Minitest::Test
   def test_log_softmax
     m = Torch::NN::LogSoftmax.new
     input = Torch.randn(2, 3)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_softmax
     m = Torch::NN::Softmax.new(dim: 1)
     input = Torch.randn(2, 3)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_softmax2d
     m = Torch::NN::Softmax2d.new
     input = Torch.randn(2, 3, 12, 13)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_softmin
     m = Torch::NN::Softmin.new
     input = Torch.randn(2, 3)
-    output = m.call(input)
+    _output = m.call(input)
   end
 end
--- test/nn/activations_test.rb
+++ test/nn/activations_test.rb
@@ -4,66 +4,66 @@ class ActivationsTest < Minitest::Test
   def test_hardshrink
     m = Torch::NN::Hardshrink.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_leaky_relu
     m = Torch::NN::LeakyReLU.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_log_sigmoid
     m = Torch::NN::LogSigmoid.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_prelu
     m = Torch::NN::PReLU.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_relu
     m = Torch::NN::ReLU.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_sigmoid
     m = Torch::NN::Sigmoid.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_softplus
     m = Torch::NN::Softplus.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_softshrink
     m = Torch::NN::Softshrink.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_softsign
     m = Torch::NN::Softsign.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_tanh
     m = Torch::NN::Tanh.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 
   def test_tanhshrink
     m = Torch::NN::Tanhshrink.new
     input = Torch.randn(2)
-    output = m.call(input)
+    _output = m.call(input)
   end
 end
