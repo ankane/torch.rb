89588a3c8d840317ab8019a70a14366c10d2a09c
Andrew Kane <andrew@ankane.org>
2022-10-30 23:15:29 -0700

    Added assert_tensor method [skip ci]

--- test/operations_test.rb
+++ test/operations_test.rb
@@ -3,19 +3,19 @@ require_relative "test_helper"
 class OperationsTest < Minitest::Test
   def test_add
     x = Torch.ones(2)
-    assert_equal [2, 2], (x + x).to_a
-    assert_equal [2, 2], x.add(x).to_a
-    assert_equal [2, 2], Torch.add(x, x).to_a
-    assert_equal [3, 3], (x + 2).to_a
+    assert_tensor [2, 2], (x + x)
+    assert_tensor [2, 2], x.add(x)
+    assert_tensor [2, 2], Torch.add(x, x)
+    assert_tensor [3, 3], (x + 2)
     x.add!(x)
-    assert_equal [2, 2], x.to_a
+    assert_tensor [2, 2], x
   end
 
   def test_add_alpha
     x = Torch.tensor([1, 2, 3])
     y = Torch.tensor([10, 20, 30])
     x.add!(y, alpha: 2)
-    assert_equal [21, 42, 63], x.to_a
+    assert_tensor [21, 42, 63], x
   end
 
   def test_mul_type
@@ -28,15 +28,15 @@ class OperationsTest < Minitest::Test
     x = Torch.tensor([1, 2, 3])
     y = x.clone
     x.add!(1)
-    assert_equal [2, 3, 4], x.to_a
-    assert_equal [1, 2, 3], y.to_a
+    assert_tensor [2, 3, 4], x
+    assert_tensor [1, 2, 3], y
   end
 
   def test_topk
     x = Torch.arange(1.0, 6.0)
     values, indices = Torch.topk(x, 3)
-    assert_equal [5, 4, 3], values.to_a
-    assert_equal [4, 3, 2], indices.to_a
+    assert_tensor [5, 4, 3], values
+    assert_tensor [4, 3, 2], indices
   end
 
   def test_add_too_many_arguments
@@ -58,23 +58,23 @@ class OperationsTest < Minitest::Test
 
   def test_cat
     x = Torch.tensor([1, 2, 3])
-    assert_equal [1, 2, 3, 1, 2, 3], Torch.cat([x, x]).to_a
+    assert_tensor [1, 2, 3, 1, 2, 3], Torch.cat([x, x])
   end
 
   def test_scalar
     x = Torch.tensor([10, 20, 30])
-    assert_equal [15, 25, 35], (x + 5).to_a
-    assert_equal [15, 25, 35], (5 + x).to_a
-    assert_equal [5, 15, 25], (x - 5).to_a
-    assert_equal [-5, -15, -25], (5 - x).to_a
-    assert_equal [50, 100, 150], (x * 5).to_a
-    assert_equal [50, 100, 150], (5 * x).to_a
-    assert_equal [2, 4, 6], (x / 5).to_a
-    assert_equal [6, 3, 2], (60 / x).to_a
-    assert_equal [1, 2, 0], (x % 3).to_a
-    assert_equal [5, 5, 25], (25 % x).to_a
-    assert_equal [100, 400, 900], (x ** 2).to_a
-    assert_equal [-10, -20, -30], (-x).to_a
+    assert_tensor [15, 25, 35], (x + 5)
+    assert_tensor [15, 25, 35], (5 + x)
+    assert_tensor [5, 15, 25], (x - 5)
+    assert_tensor [-5, -15, -25], (5 - x)
+    assert_tensor [50, 100, 150], (x * 5)
+    assert_tensor [50, 100, 150], (5 * x)
+    assert_tensor [2, 4, 6], (x / 5)
+    assert_tensor [6, 3, 2], (60 / x)
+    assert_tensor [1, 2, 0], (x % 3)
+    assert_tensor [5, 5, 25], (25 % x)
+    assert_tensor [100, 400, 900], (x ** 2)
+    assert_tensor [-10, -20, -30], (-x)
   end
 
   def test_scalar_dtype
@@ -104,27 +104,27 @@ class OperationsTest < Minitest::Test
   def test_eq
     x = Torch.tensor([[1, 2], [3, 4]])
     y = Torch.tensor([[1, 1], [4, 4]])
-    assert_equal [[true, false], [false, true]], Torch.eq(x, y).to_a
-    assert_equal [[1, 0], [0, 1]], Torch.eq(x, y).uint8.to_a
+    assert_tensor [[true, false], [false, true]], Torch.eq(x, y)
+    assert_tensor [[1, 0], [0, 1]], Torch.eq(x, y).uint8
   end
 
   def test_flatten
     x = Torch.tensor([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
-    assert_equal [1, 2, 3, 4, 5, 6, 7, 8], Torch.flatten(x).to_a
-    assert_equal [[1, 2, 3, 4], [5, 6, 7, 8]], Torch.flatten(x, start_dim: 1).to_a
+    assert_tensor [1, 2, 3, 4, 5, 6, 7, 8], Torch.flatten(x)
+    assert_tensor [[1, 2, 3, 4], [5, 6, 7, 8]], Torch.flatten(x, start_dim: 1)
   end
 
   def test_setter_tensor_float
     x = Torch.tensor([1.0, 2, 3])
     index = Torch.tensor([false, true, false])
     x[index] = 9
-    assert_equal [1, 9, 3], x.to_a
+    assert_tensor [1, 9, 3], x
   end
 
   def test_clamp
     x = Torch.tensor([1, 2, 3, 4, 5])
     x.clamp!(2, 4)
-    assert_equal [2, 2, 3, 4, 4], x.to_a
+    assert_tensor [2, 2, 3, 4, 4], x
   end
 
   def test_transpose
@@ -173,26 +173,24 @@ class OperationsTest < Minitest::Test
 
   def test_select
     x = Torch.arange(0, 100).view([10, 10])
-    assert_equal [3, 13, 23, 33, 43, 53, 63, 73, 83, 93], x.select(1, 3).to_a
+    assert_tensor [3, 13, 23, 33, 43, 53, 63, 73, 83, 93], x.select(1, 3)
   end
 
   def test_narrow
     x = Torch.arange(0, 100).view([10, 10])
     expected = [[3, 4], [13, 14], [23, 24], [33, 34], [43, 44], [53, 54], [63, 64], [73, 74], [83, 84], [93, 94]]
-    assert_equal expected, x.narrow(1, 3, 2).to_a
+    assert_tensor expected, x.narrow(1, 3, 2)
   end
 
   def test_hann_window
-    assert_equal [1], Torch.hann_window(1).to_a
-    assert_equal [0, 1], Torch.hann_window(2).to_a
-    assert_equal :float32, Torch.hann_window(1).dtype
+    assert_tensor [1], Torch.hann_window(1), dtype: :float32
+    assert_tensor [0, 1], Torch.hann_window(2)
   end
-
 
   def test_fill_diagonal
     x = Torch.ones(2, 2)
-    assert_equal [[3, 1], [1, 3]], x.fill_diagonal!(3).to_a
-    assert_equal [[3, 1], [1, 3]], x.to_a
+    assert_tensor [[3, 1], [1, 3]], x.fill_diagonal!(3)
+    assert_tensor [[3, 1], [1, 3]], x
   end
 
   def test_chunk
@@ -235,8 +233,8 @@ class OperationsTest < Minitest::Test
 
   def test_permute
     x = Torch.tensor([[1, 2, 3]])
-    assert_equal [[1], [2], [3]], x.permute([1, 0]).to_a
-    assert_equal [[1], [2], [3]], x.permute(1, 0).to_a
+    assert_tensor [[1], [2], [3]], x.permute([1, 0])
+    assert_tensor [[1], [2], [3]], x.permute(1, 0)
   end
 
   def test_permute_bad
@@ -249,7 +247,7 @@ class OperationsTest < Minitest::Test
 
   def test_new_full
     x = Torch.tensor([[1, 2, 3]])
-    assert_equal [[0, 0], [0, 0]], x.new_full([2, 2], 0).to_a
+    assert_tensor [[0, 0], [0, 0]], x.new_full([2, 2], 0)
   end
 
   def test_divide_rounding_mode_floor
@@ -264,28 +262,28 @@ class OperationsTest < Minitest::Test
   def test_divide_rounding_mode_nil
     a = Torch.full([2], 4.2)
     b = Torch.full([2], 2)
-    assert_elements_in_delta [2.1, 2.1], Torch.divide(a, b, rounding_mode: nil).to_a
+    assert_tensor [2.1, 2.1], Torch.divide(a, b, rounding_mode: nil)
   end
 
   def test_floor_divide
     a = Torch.tensor([4.0, -3.0])
     b = Torch.tensor([2.0, 2.0])
-    assert_elements_in_delta [2.0, -2.0], Torch.floor_divide(a, b).to_a
-    assert_elements_in_delta [2.0, -1.0], Torch.div(a, b, rounding_mode: "trunc").to_a
+    assert_tensor [2.0, -2.0], Torch.floor_divide(a, b)
+    assert_tensor [2.0, -1.0], Torch.div(a, b, rounding_mode: "trunc")
   end
 
   # TODO raise
   # RuntimeError: value cannnot be converted to type int8 without overflow
   def test_tensor_overflow
-    assert_equal [-24], Torch.tensor(1000, dtype: :int8).to_a
+    assert_tensor [-24], Torch.tensor(1000, dtype: :int8)
   end
 
   def test_left_shift
-    assert_equal [128], (Torch.tensor([64]) << 1).to_a
+    assert_tensor [128], (Torch.tensor([64]) << 1)
   end
 
   def test_right_shift
-    assert_equal [32], (Torch.tensor([64]) >> 1).to_a
+    assert_tensor [32], (Torch.tensor([64]) >> 1)
   end
 
   def test_all
@@ -298,7 +296,7 @@ class OperationsTest < Minitest::Test
     y.add!(2)
     # matches behavior of PyTorch
     # incorrect value in 1.10.0 release notes
-    assert_equal [3 + 2i], x.to_a
+    assert_tensor [3 + 2i], x
   end
 
   def test_conj_neg
@@ -307,7 +305,7 @@ class OperationsTest < Minitest::Test
     z = y.imag
     assert z.neg?
     z.add!(2)
-    assert_equal [1 - 0i], x.to_a
+    assert_tensor [1 - 0i], x
   end
 
   def test_clamp_dtype
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,6 +19,16 @@ class Minitest::Test
     end
   end
 
+  def assert_tensor(expected, actual, dtype: nil)
+    assert_kind_of Torch::Tensor, actual
+    assert_equal actual.dtype, dtype if dtype
+    if actual.floating_point? && actual.dim < 2
+      assert_elements_in_delta expected, actual.to_a
+    else
+      assert_equal expected, actual.to_a
+    end
+  end
+
   def mac?
     RbConfig::CONFIG["host_os"] =~ /darwin/i
   end
