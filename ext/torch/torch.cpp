e3b38de0f8e67c5a13e63f437b8f9aa4c8803b6c
Andrew Kane <andrew@ankane.org>
2024-11-10 20:34:24 -0800

    Simplified code

--- ext/torch/torch.cpp
+++ ext/torch/torch.cpp
@@ -16,13 +16,7 @@ torch::Tensor make_tensor(Rice::Array a, const std::vector<int64_t> &size, const
     vec.push_back(Rice::detail::From_Ruby<T>().convert(a[i].value()));
   }
 
-  // hack for requires_grad error
-  auto requires_grad = options.requires_grad();
-  torch::Tensor t = torch::tensor(vec, options.requires_grad(c10::nullopt));
-  if (requires_grad) {
-    t.set_requires_grad(true);
-  }
-
+  torch::Tensor t = torch::tensor(vec, options);
   return t.reshape(size);
 }
 
